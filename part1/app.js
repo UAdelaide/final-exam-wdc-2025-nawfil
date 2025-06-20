f88cd497d29688b449184195ee8589813726a516
nawfil <nawfilhossain155@gmail.com>
2025-06-20 06:47:12 +0000

    [automatic commit] 2025-06-20 06:47:12 UTC 101.115.235.115

--- part1/app.js
+++ part1/app.js
@@ -43,7 +43,7 @@ let db;
       `);
 
       await db.execute(`
-        INSERT INTO WalkRequests (dog_id, datetime, duration_minutes, location, status)
+        INSERT INTO WalkRequests (dog_id, date_time, duration_minutes, location, status)
         VALUES
           ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
           ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
