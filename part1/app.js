18d4245a3970294edfcf5ee3149786191f052824
nawfil <nawfilhossain155@gmail.com>
2025-06-20 07:36:57 +0000

    [automatic commit] 2025-06-20 07:36:57 UTC 101.115.235.115

--- part1/app.js
+++ part1/app.js
@@ -51,8 +51,7 @@ let db;
             ((SELECT dog_id FROM Dogs WHERE name='Luna'), '2025-06-11 14:30:00', 20, 'Riverbank Walk', 'open'),
             ((SELECT dog_id FROM Dogs WHERE name='Coco'), '2025-06-12 07:00:00', 40, 'Botanic Gardens', 'open')
         );
-
-
+        
       console.log('✅ Test data inserted.');
     }
   } catch (err) {
