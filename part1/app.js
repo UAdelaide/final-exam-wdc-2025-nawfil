fa5f5a286bfc147ba9125557e1cc278f724e99c4
nawfil <nawfilhossain155@gmail.com>
2025-06-20 07:36:16 +0000

    [automatic commit] 2025-06-20 07:36:16 UTC 101.115.235.115

--- part1/app.js
+++ part1/app.js
@@ -45,11 +45,11 @@ let db;
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES
-    ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-    ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-11 10:00:00', 60, 'Hilltop Trail', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name='Luna'), '2025-06-11 14:30:00', 20, 'Riverbank Walk', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name='Coco'), '2025-06-12 07:00:00', 40, 'Botanic Gardens', 'open')
+            ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-11 10:00:00', 60, 'Hilltop Trail', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name='Luna'), '2025-06-11 14:30:00', 20, 'Riverbank Walk', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name='Coco'), '2025-06-12 07:00:00', 40, 'Botanic Gardens', 'open')
 `);
 
 
