9d52720694123033a1a6b053be54fca84d9200c7
nawfil <nawfilhossain155@gmail.com>
2025-06-21 05:13:46 +0000

    [automatic commit] 2025-06-21 05:13:46 UTC 101.115.226.251

--- part2/routes/dogroutes.js
+++ part2/routes/dogroutes.js
@@ -1,7 +1,22 @@
 // routes/dogRoutes.js
 const express = require('express');
-const db = require('../models/db'); 
+const db = require('../models/db');
 const router = express.Router();
+
+// GET dogs owned by the logged-in user
+router.get('/mydogs', async (req, res) => {
+    if (!req.session.user) {
+      return res.status(401).json({ error: 'Not logged in' });
+    }
+
+    const ownerId = req.session.user.user_id;
+    try {
+      const [dogs] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]);
+      res.json(dogs);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to load dogs' });
+    }
+  });
 
 // GET all dogs with owner_id
 router.get('/', async (req, res) => {
